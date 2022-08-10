3784f59e0d7efa61a2915af127d51a5d19f7b956
anik <anikbhadra01@gmail.com>
2022-08-10 11:28:15 +0530

    reviews prev and next person

--- src/reviewsProject/Review.jsx
+++ src/reviewsProject/Review.jsx
@@ -6,6 +6,29 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 function Review() {
     const [index, setIndex] = useState(0)
     const { name, job, image, text } = people[index]
+
+    const checkNumber = (number) => {
+        if (number > people.length - 1) {
+            return 0
+        }
+        if (number < 0) {
+            return people.length - 1
+        }
+        return number
+    }
+
+    const nextPerson = () => {
+        setIndex((index) => {
+            const newIndex = index + 1
+            return checkNumber(newIndex)
+        })
+    }
+    const prevPerson = () => {
+        setIndex((index) => {
+            const newIndex = index - 1
+            return checkNumber(newIndex)
+        })
+    }
     return (
         <article>
             <img src={image} alt={name} />
@@ -14,10 +37,10 @@ function Review() {
             <p>{text}</p>
 
             <div>
-                <button>
+                <button onClick={prevPerson}>
                     <FaChevronLeft />
                 </button>
-                <button>
+                <button onClick={nextPerson}>
                     <FaChevronRight />
                 </button>
             </div>
